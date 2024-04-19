345e93471152443f13fca5aefad91ad99d6cf7c4
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-04-19 16:07:19 +0500

    update changes

--- src/components/Globel/Text.js
+++ src/components/Globel/Text.js
@@ -1,111 +1,102 @@
 const IndexPagePic =
-	"https://res.cloudinary.com/hamzanafasat/image/upload/v1704068266/portfolio%20website/u8tenv9sg4vmxxt6a9xz.png";
+    "https://res.cloudinary.com/hamzanafasat/image/upload/v1704068266/portfolio%20website/u8tenv9sg4vmxxt6a9xz.png";
 const AboutPageDp =
-	"https://res.cloudinary.com/hamzanafasat/image/upload/v1704067146/portfolio%20website/rh73wahkxdruyvb12jzr.jpg";
-import logo from "../../../public/logo.png";
+    "https://res.cloudinary.com/hamzanafasat/image/upload/v1713523995/portfolio%20website/rhepqqg3s6hovmxp79wq.jpg";
 const ProfileText_1 =
-	"Hey everyone! I am Hamza Nafasat, a Full Stack Developer. I love making website's look cool and work well. I am expert in HTML, SASS, Tailwind, JavaScript, React, for creating amazing sites.";
+    "Hey everyone! I am Hamza Nafasat, a Full Stack Developer. I love making website's look cool and work well. I am expert in HTML, SASS, Tailwind, JavaScript, React, for creating amazing sites.";
 const ProfileText_2 =
-	"But coding's just half of the story! I'm a problem solver! I transform your ideas into digital reality. My knowledge thirst keeps me exploring new skills, like Node.js, Express, MongoDB";
+    "But coding's just half of the story! I'm a problem solver! I transform your ideas into digital reality. My knowledge thirst keeps me exploring new skills, like Node.js, Express, MongoDB";
 const ProfileText_3 =
-	"Security? My middle name! I build secure logins and protect your info like a personal gaurd.  Basically, I'm your web partner, handling everything from the basics to the coolest trends. Let's chat about your project and make something great!";
-export { IndexPagePic, AboutPageDp, logo, ProfileText_1, ProfileText_2, ProfileText_3 };
+    "Security? My middle name! I build secure logins and protect your info like a personal gaurd.  Basically, I'm your web partner, handling everything from the basics to the coolest trends. Let's chat about your project and make something great!";
+export { IndexPagePic, AboutPageDp, ProfileText_1, ProfileText_2, ProfileText_3 };
 
 export const SIMPLEOVERVIEW = [
-	{ text: "Adapted Skills", num: 15 },
-	{ text: "Projects Completed", num: 10 },
-	{ text: "Years Of Experience", num: 1 },
+    { text: "Adapted Skills", num: 20 },
+    { text: "Projects Completed", num: 10 },
+    { text: "Years Of Experience", num: 1.5 },
 ];
 // EDUCATION AND EXPERIENCES
 // =========================
 export const EXPERIENCES = [
-	{
-		position: "Software Engineer",
-		company: "Google",
-		companyLink: "https://www.google.com/",
-		time: "2022-Present",
-		address: "Mountain View CA",
-		work: "Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and developing new tools for data analysis and visualization",
-	},
-
-	{
-		position: "Teaching Assistant",
-		company: "Facebook",
-		companyLink: "https://www.facebook.com/",
-		time: "2022-Present",
-		address: "Menlo Park CA",
-		work: "Contributed to the development of Facebook's social media platform, focusing on user experience and implementing new features to enhance user engagement.",
-	},
-	{
-		position: "Software Developer",
-		company: "Microsoft",
-		companyLink: "https://www.microsoft.com/",
-		time: "2022-Present",
-		address: "Redmond WA",
-		work: "Worked on projects related to Microsoft's cloud services and software development tools, collaborating with cross-functional teams to deliver high-quality solutions.",
-	},
-	{
-		position: "Intern",
-		company: "Netflix",
-		companyLink: "https://www.netflix.com/",
-		time: "2022-Present",
-		address: "Los Gatos CA",
-		work: "Contributed to the development of streaming technologies at Netflix, optimizing video delivery and enhancing the overall user experience for millions of subscribers worldwide.",
-	},
+    {
+        position: "Full Stack Developer",
+        company: "Acme Digital Solution",
+        companyLink: "#",
+        time: "2023-Present",
+        address: "Lahore, Pakistan",
+        work: "I design and develop full-stack web applications, leveraging front-end, back-end, and database expertise. Prioritize user-centric design principles to deliver intuitive and engaging web app experiences. Champion security through robust measures like secure authentication, authorization, and data encryption to safeguard user privacy",
+    },
+    {
+        position: "Free Lancer",
+        company: "Online Clients",
+        companyLink: "#",
+        time: "2023-2024",
+        address: "Gujrat, Pakistan",
+        work: "As a Free Lancer in Full-Stack Web Developer, I specialize in creating intuitive and secure web applications. My expertise spans both front-end and back-end development, ensuring seamless user experiences. Let’s bring your vision to life! ",
+    },
 ];
 export const EDUCATION = [
-	{
-		type: "Master's (MA) in Arabic",
-		time: "2020",
-		place: "Islamic University Mukarram Mosque, Gujranwala",
-		info: "I have done my Master of Arts (MA) degree in Islamic and Arabic Studies from Wafaq ul Madaris Al Salfia. This degree is recognized by the Higher Education Commission (HEC) and the Lahore High Court as a valid qualification for pursuing higher education or employment in Pakistan.",
-	},
-	{
-		type: "Online Courses",
-		time: "2022 - Present",
-		place: "Youtube and Google tutorials",
-		info: "Completing a self-learning journey, mastering advanced technologies, including the entire MERN stack, along with SCSS and TypeScript, demonstrates my adaptability to the dynamic tech landscape.",
-	},
-	{
-		type: "Intermediate Certificate",
-		time: "",
-		place: "Private Candidate",
-		info: "Demonstrated academic proficiency by successfully completing the Intermediate examination as a private candidate from the esteemed Gujranwala Board, showcasing dedication and self-motivation in the pursuit of education.",
-	},
+    {
+        type: "Master's (MA) in Arabic",
+        time: "2020",
+        place: "Islamic University Mukarram Mosque, Gujranwala",
+        info: "I have done my Master of Arts (MA) degree in Islamic and Arabic Studies from Wafaq ul Madaris Al Salfia. This degree is recognized by the Higher Education Commission (HEC) and the Lahore High Court as a valid qualification for pursuing higher education or employment in Pakistan.",
+    },
+    // {
+    //     type: "Online Courses",
+    //     time: "2022 - Present",
+    //     place: "Youtube and Google tutorials",
+    //     info: "Completing a self-learning journey, mastering advanced technologies, including the entire MERN stack, along with SCSS and TypeScript, demonstrates my adaptability to the dynamic tech landscape.",
+    // },
+    {
+        type: "Intermediate Certificate",
+        time: "",
+        place: "Private Candidate",
+        info: "Demonstrated academic proficiency by successfully completing the Intermediate examination as a private candidate from the esteemed Gujranwala Board, showcasing dedication and self-motivation in the pursuit of education.",
+    },
 ];
 // PROJECTS
 // ========
 export const FEATUREDPROJECTDATA = [
-	{
-		type: "MERN Website",
-		title: "CodeNinja",
-		link: "https://codeninja-mern.vercel.app",
-		github: "https://github.com/hamzanafasat/codeninja-mern-frontend",
-		image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1704933284/portfolio%20website/fkfzvqoskq6ohpgg7vwi.png",
-		summery:
-			"A MERN stack online learning platform with subscription-based lectures, secure authentication, theme modes, and an admin dashboard for real-time data visualization.",
-	},
+    {
+        type: "MERN Website",
+        title: "E-Commerce-Store",
+        link: "https://mern-e-commerce-2024.vercel.app/",
+        github: "https://github.com/hamza-nafasat/my-projects/tree/main/mern/e-commerece",
+        image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1713523493/portfolio%20website/shonrfdg7tlemdothtjf.png",
+        summery:
+            "Developed a scalable e-commerce platform for seamless product browsing and purchasing. Implemented a data-rich dashboard with charts and tables for website insights. Secure payment processing via Stripe and Firebase authentication.",
+    },
+    {
+        type: "MERN Website",
+        title: "CodeNinja",
+        link: "https://github.com/hamza-nafasat/my-projects/tree/main/mern/code-ninja",
+        github: "https://github.com/hamza-nafasat/my-projects/tree/main/mern/code-ninja",
+        image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1704933284/portfolio%20website/fkfzvqoskq6ohpgg7vwi.png",
+        summery:
+            "A MERN stack online learning platform with subscription-based lectures, secure authentication, theme modes, and an admin dashboard for real-time data visualization.",
+    },
 ];
 export const SMAILLPROJECTDATA = [
-	{
-		type: "Static Landing Page",
-		title: "Barber Shop",
-		link: "https://barber-website-static.vercel.app",
-		github: "https://github.com/hamzanafasat/barber-website-static",
-		image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1704931293/portfolio%20website/ktpo5zdvqogaetiqekce.png",
-	},
-	{
-		type: "React Practice App",
-		title: "Blogging Website",
-		link: "https://blooging-webpage-react.vercel.app",
-		github: "https://github.com/hamzanafasat/blooging-webpage-react",
-		image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1704933280/portfolio%20website/vqtxp8ata0yu4q1suevt.png",
-	},
-	{
-		type: "Beginners React App",
-		title: "Todo List",
-		link: "https://biginer-todo-list.vercel.app",
-		github: "https://github.com/hamzanafasat/todo-list-reactjs",
-		image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1704933274/portfolio%20website/wgvuv4qwaa6yqimnr21z.png",
-	},
+    {
+        type: "Static Landing Page",
+        title: "Barber Shop",
+        link: "https://barber-website-static.vercel.app",
+        github: "https://github.com/hamzanafasat/barber-website-static",
+        image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1704931293/portfolio%20website/ktpo5zdvqogaetiqekce.png",
+    },
+    {
+        type: "React Practice App",
+        title: "Static Blogging Page",
+        link: "https://blooging-webpage-react.vercel.app",
+        github: "https://github.com/hamzanafasat/blooging-webpage-react",
+        image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1704933280/portfolio%20website/vqtxp8ata0yu4q1suevt.png",
+    },
+    // {
+    //     type: "Beginners React App",
+    //     title: "Todo List",
+    //     link: "https://biginer-todo-list.vercel.app",
+    //     github: "https://github.com/hamzanafasat/todo-list-reactjs",
+    //     image: "https://res.cloudinary.com/hamzanafasat/image/upload/v1704933274/portfolio%20website/wgvuv4qwaa6yqimnr21z.png",
+    // },
 ];
--- src/pages/about.js
+++ src/pages/about.js
@@ -9,107 +9,102 @@ import TransitionEffect from "@/components/Globel/TransitionEffect";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 import { SIMPLEOVERVIEW, AboutPageDp } from "@/components/Globel/Text";
 import { ProfileText_2, ProfileText_1, ProfileText_3 } from "@/components/Globel/Text";
+import Experience from "@/components/About/Experience";
 
 const AnimatedNumber = ({ value }) => {
-	const ref = useRef(null);
-	const motionValue = useMotionValue(0);
-	const springValue = useSpring(motionValue, { duration: 3000 });
-	const isInView = useInView(ref, { once: true });
-	useEffect(() => {
-		if (isInView) {
-			motionValue.set(value);
-		}
-	}, [isInView, value, motionValue]);
-	useEffect(() => {
-		springValue.on("change", (latest) => {
-			if (ref.current && latest.toFixed(0) <= value) {
-				ref.current.textContent = latest.toFixed(0);
-			}
-		});
-	}, [springValue, value]);
-	return <span ref={ref} />;
+    const ref = useRef(null);
+    const motionValue = useMotionValue(0);
+    const springValue = useSpring(motionValue, { duration: 3000 });
+    const isInView = useInView(ref, { once: true });
+    useEffect(() => {
+        if (isInView) {
+            motionValue.set(value);
+        }
+    }, [isInView, value, motionValue]);
+    useEffect(() => {
+        springValue.on("change", (latest) => {
+            if (ref.current && latest.toFixed(0) <= value) {
+                ref.current.textContent = latest.toFixed(0);
+            }
+        });
+    }, [springValue, value]);
+    return <span ref={ref} />;
 };
 
 const about = () => {
-	return (
-		<>
-			<Head>
-				<title>Portfoli | About</title>
-				<meta name="description" content="the page about hamzanafasat" />
-			</Head>
-			<TransitionEffect />
-			<main className="flex w-full flex-col items-center justify-center dark:text-light">
-				<Layout classname="pt-0 xl:pt-0 lg:pt-0 md:pt-0 sm:pt-0">
-					<AnimatedText
-						text={"Coding Journey"}
-						className="!mb-16 lg:!text-7xl sm:!text-5xl xs:!text-3xl sm:!mb-4"
-					/>
-					<div className="grid w-full grid-cols-8 gap-16 sm:gap-8 ">
-						{/* PROFILE TEXT  */}
-						{/* ============ */}
-						<div
-							className="col-span-3 xl:col-span-5 flex flex-col items-start text-left
+    return (
+        <>
+            <Head>
+                <title>Portfoli | About</title>
+                <meta name="description" content="the page about hamzanafasat" />
+            </Head>
+            <TransitionEffect />
+            <main className="flex w-full flex-col items-center justify-center dark:text-light">
+                <Layout classname="pt-0 xl:pt-0 lg:pt-0 md:pt-0 sm:pt-0">
+                    <AnimatedText
+                        text={"Coding Journey"}
+                        className="!mb-16 lg:!text-7xl sm:!text-5xl xs:!text-3xl sm:!mb-4"
+                    />
+                    <div className="grid w-full grid-cols-8 gap-16 sm:gap-8 ">
+                        {/* PROFILE TEXT  */}
+                        {/* ============ */}
+                        <div
+                            className="col-span-3 xl:col-span-5 flex flex-col items-start text-left
 							lg:text-justify justify-start md:order-2 md:col-span-8 lg:text-sm"
-						>
-							<h2
-								className="mb-4 text-lg font-bold uppercase text-dark/75
+                        >
+                            <h2
+                                className="mb-4 text-lg font-bold uppercase text-dark/75
 							dark:text-light/75"
-							>
-								Biography
-							</h2>
-							<p>{ProfileText_1}</p>
-							<p className="my-4">{ProfileText_2}</p>
-							<p>{ProfileText_3}</p>
-						</div>
-						{/* PROFILE IMAGE  */}
-						{/* ============== */}
-						<div
-							className="col-span-3 xl:col-span-3 relative h-max   rounded-2xl border border-solid
+                            >
+                                Biography
+                            </h2>
+                            <p>{ProfileText_1}</p>
+                            <p className="my-4">{ProfileText_2}</p>
+                            <p>{ProfileText_3}</p>
+                        </div>
+                        {/* PROFILE IMAGE  */}
+                        {/* ============== */}
+                        <div
+                            className="col-span-3 xl:col-span-3 relative h-max   rounded-2xl border border-solid
 							border-dark p-8 dark:border-light bg-light dark:bg-dark 
 							md:order-1 md:col-span-8 xl:mt-10 md:mx-[30%] sm:mx-[20%] xs:mx-4"
-						>
-							<div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
-							<Image
-								src={AboutPageDp}
-								alt="Hamza Nafasat"
-								width={500}
-								height={500}
-								priority
-							/>
-						</div>
-						{/* PROFILE DETAILS  */}
-						{/* ================ */}
-						<div
-							className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row md:order-3
+                        >
+                            <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+                            <Image src={AboutPageDp} alt="Hamza Nafasat" width={500} height={500} priority />
+                        </div>
+                        {/* PROFILE DETAILS  */}
+                        {/* ================ */}
+                        <div
+                            className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row md:order-3
 						xl:items-center"
-						>
-							{SIMPLEOVERVIEW.map((item, i) => (
-								<div
-									key={i + "fsakdf" + 12 + i}
-									className="flex flex-col items-end justify-center xl:items-center"
-								>
-									<span className="inline-block text-7xl font-bold sm:text-5xl xs:text-4xl">
-										<AnimatedNumber value={item.num} />+
-									</span>
-									<h2
-										className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center 
+                        >
+                            {SIMPLEOVERVIEW.map((item, i) => (
+                                <div
+                                    key={i + "fsakdf" + 12 + i}
+                                    className="flex flex-col items-end justify-center xl:items-center"
+                                >
+                                    <span className="inline-block text-7xl font-bold sm:text-5xl xs:text-4xl">
+                                        <AnimatedNumber value={item.num} />+
+                                    </span>
+                                    <h2
+                                        className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center 
 										md:text-lg sm:text-base xs:text-xs mb-4"
-									>
-										{item.text}
-									</h2>
-								</div>
-							))}
-						</div>
-					</div>
-					{/* OTHER DETAILS  */}
-					{/* ============== */}
-					<Skills />
-					{/* <Experience /> */}
-					<Education />
-				</Layout>
-			</main>
-		</>
-	);
+                                    >
+                                        {item.text}
+                                    </h2>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    {/* OTHER DETAILS  */}
+                    {/* ============== */}
+                    <Skills />
+                    <Experience />
+                    <Education />
+                </Layout>
+            </main>
+        </>
+    );
 };
 
 export default about;
