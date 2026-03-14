2447af158f2b5020ad7bb8234a14c97fb7a68584
Hamza Nafasat <149063128+hamza-nafasat@users.noreply.github.com>
2026-03-15 01:52:44 +0500

    Update AboutPageDp image URL in Text.js

--- src/components/Globel/Text.js
+++ src/components/Globel/Text.js
@@ -4,7 +4,7 @@ const IndexPagePic =
 //   "https://res.cloudinary.com/hamzanafasat/image/upload/v1733300985/portfolio%20website/whmqwhswgrap4enqiwko.jpg";
 const AboutPageDp =
   // "https://res.cloudinary.com/hamzanafasat/image/upload/v1767246947/portfolio%20website/DevImage_ozuegx.jpg";
-  "https://res.cloudinary.com/hamzanafasat/image/upload/v1773450036/portfolio%20website/black-lenthin_hlnmj2.jpg"
+  "https://res.cloudinary.com/hamzanafasat/image/upload/v1773521355/portfolio%20website/file_000000005f1471fa9953d5d81c43d797_skkt7u.png"
 const ProfileText_1 =
   "I’m Hamza Nafasat, a full-stack SaaS developer specializing in building secure, scalable, and production-ready web applications. I design and engineer SaaS platforms that handle real users, real data, and real business constraints—not experimental prototypes.";
 
